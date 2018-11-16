a77bc79fddba8a8ac00b762142640a68fc133dec
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-16 23:55:50 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author 
+  attr_accessor :name, :posts
+  
+  @@posts = []
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def posts 
+    @@posts
+  end 
+  
+  def add_post(post)
+    self.posts = post
+    @@posts << self
+  end 
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self 
+    self.posts = post
+    @@posts << post
+  end 
+  
+  def self.post_count 
+    @@posts.length
+  end 
+  
+end 
